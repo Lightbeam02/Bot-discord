668b22e88a0e06228382ad1b2daff67c8104193f
Ahmad Lutfi <96603202+Lightbeam02@users.noreply.github.com>
2022-11-14 11:20:08 +0000

    Initial commit

---
+++ public/func/submission-created.js
@@ -0,0 +1,112 @@
+const fetch = require("node-fetch");
+
+const { API_ENDPOINT, MAX_EMBED_FIELD_CHARS, MAX_EMBED_FOOTER_CHARS } = require("./helpers/discord-helpers.js");
+const { createJwt, decodeJwt } = require("./helpers/jwt-helpers.js");
+const { getBan } = require("./helpers/user-helpers.js");
+
+exports.handler = async function (event, context) {
+    let payload;
+
+    if (process.env.USE_NETLIFY_FORMS) {
+        payload = JSON.parse(event.body).payload.data;
+    } else {
+        if (event.httpMethod !== "POST") {
+            return {
+                statusCode: 405
+            };
+        }
+
+        const params = new URLSearchParams(event.body);
+        payload = {
+            banReason: params.get("banReason") || undefined,
+            appealText: params.get("appealText") || undefined,
+            futureActions: params.get("futureActions") || undefined,
+            token: params.get("token") || undefined
+        };
+    }
+
+    if (payload.banReason !== undefined &&
+        payload.appealText !== undefined &&
+        payload.futureActions !== undefined && 
+        payload.token !== undefined) {
+        
+        const userInfo = decodeJwt(payload.token);
+        const message = {
+            embed: {
+                title: "New appeal submitted!",
+                timestamp: new Date().toISOString(),
+                fields: [
+                    {
+                        name: "Submitter",
+                        value: `<@${userInfo.id}> (${userInfo.username}#${userInfo.discriminator})`
+                    },
+                    {
+                        name: "Why were you banned?",
+                        value: payload.banReason.slice(0, MAX_EMBED_FIELD_CHARS)
+                    },
+                    {
+                        name: "Why do you feel you should be unbanned?",
+                        value: payload.appealText.slice(0, MAX_EMBED_FIELD_CHARS)
+                    },
+                    {
+                        name: "What will you do to avoid being banned in the future?",
+                        value: payload.futureActions.slice(0, MAX_EMBED_FIELD_CHARS)
+                    }
+                ]
+            }
+        }
+
+        if (process.env.GUILD_ID) {
+            try {
+                const ban = await getBan(userInfo.id, process.env.GUILD_ID, process.env.DISCORD_BOT_TOKEN);
+                if (ban !== null && ban.reason) {
+                    message.embed.footer = {
+                        text: `Original ban reason: ${ban.reason}`.slice(0, MAX_EMBED_FOOTER_CHARS)
+                    };
+                }
+            } catch (e) {
+                console.log(e);
+            }
+
+            if (!process.env.DISABLE_UNBAN_LINK) {
+                const unbanUrl = new URL("/.netlify/functions/unban", process.env.URL);
+                const unbanInfo = {
+                    userId: userInfo.id
+                };
+    
+                message.embed.description = `[Approve appeal and unban user](${unbanUrl.toString()}?token=${encodeURIComponent(createJwt(unbanInfo))})`;
+            }
+        }
+
+        const result = await fetch(`${API_ENDPOINT}/channels/${encodeURIComponent(process.env.APPEALS_CHANNEL)}/messages`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bot ${process.env.DISCORD_BOT_TOKEN}`
+            },
+            body: JSON.stringify(message)
+        });
+
+        if (result.ok) {
+            if (process.env.USE_NETLIFY_FORMS) {
+                return {
+                    statusCode: 200
+                };
+            } else {
+                return {
+                    statusCode: 303,
+                    headers: {
+                        "Location": "/success"
+                    }
+                };
+            }
+        } else {
+            console.log(await result.json());
+            throw new Error("Failed to submit message");
+        }
+    }
+
+    return {
+        statusCode: 400
+    };
+}
